22bac8c7e47f522037893a98b61fd103941306e5
Maksim Pokrovskii <pokrovsky89@gmail.com>
2021-03-06 21:36:16 +0200

    Update myKatas.js

--- myKatas.js
+++ myKatas.js
@@ -30,3 +30,28 @@ function getSum(a, b) {
     const sum = (accumulator, currentValue) => accumulator + currentValue;
     return arr.reduce(sum)
 }
+
+//Formating phone number
+// input [1,2,3,4,5,6,7,8,9,0]
+
+function createPhoneNumber(numbers) {
+    var format = "(xxx) xxx-xxxx";
+
+    for (var i = 0; i < numbers.length; i++) {
+        format = format.replace('x', numbers[i]);
+    }
+
+    return format;
+}
+
+
+// Unique In Order
+var uniqueInOrder = function(iterable) {
+    let result = []
+    for (let i=0; i<iterable.length; i++){
+        if (iterable[i] != iterable[i+1]){
+            result.push(iterable[i])
+        }
+    }
+    return result
+}
